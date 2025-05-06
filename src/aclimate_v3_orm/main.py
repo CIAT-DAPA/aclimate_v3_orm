76807ffb96805c2ca62322dac841dc2fe9470109
santiagoC <s.calderon@cgiar.org>
2025-05-06 11:02:27 -0500

    import

--- src/aclimate_v3_orm/main.py
+++ src/aclimate_v3_orm/main.py
@@ -1,13 +1,8 @@
 from database import engine
 from database.base import Base
-from models.administrative.admin1 import Admin1
-from models.administrative.location import Location
-from models.administrative.country import Country
-from models.administrative.admin2 import Admin2
-from models.catalog.climate_measure import ClimateMeasure
-from models.climate.historical_climatology import ClimateHistoricalClimatology
-from models.climate.historical_daily import ClimateHistoricalDaily
-from models.climate.historical_monthly import ClimateHistoricalMonthly
+from models.administrative import Country, Admin1, Admin2, Location
+from models.catalog import ClimateMeasure
+from models.climate import ClimateHistoricalClimatology, ClimateHistoricalDaily, ClimateHistoricalMonthly
 
 # Crea todas las tablas
 Base.metadata.create_all(bind=engine)
