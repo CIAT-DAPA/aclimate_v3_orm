ed81aff001141e6f356455cf587b8a82c4a1ca11
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2025-10-09 15:41:09 -0500

    Update role.py

--- src/aclimate_v3_orm/models/role.py
+++ src/aclimate_v3_orm/models/role.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, Boolean, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
 from ..database.base import Base
 from ..enums import Apps
 
@@ -9,6 +10,9 @@ class Role(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     app = Column(Enum(Apps), nullable=False)
+    enable = Column(Boolean, default=True)
+    register = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
     users = relationship('User', back_populates='role')
     accesses = relationship('UserAccess', back_populates='role', cascade='all, delete')
