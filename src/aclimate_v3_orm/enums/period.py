ca205ad0e4d72d28b1d4d318e430f1dc6e734e3d
santiagoC <s.calderon@cgiar.org>
2026-02-23 10:48:19 -0500

    Add 'multiyear_monthly' to Period enum and create migration for schema update

--- src/aclimate_v3_orm/enums/period.py
+++ src/aclimate_v3_orm/enums/period.py
@@ -4,6 +4,7 @@ class Period(str, Enum):
     DAILY = "daily"
     MONTHLY = "monthly"
     ANNUAL = "annual"
+    MULTIYEAR_MONTHLY = "multiyear_monthly"
     SEASONAL = "seasonal"
     DECADAL = "decadal"
     OTHER = "other"
